4a7ce3568b5f3011c3ec2afd6c067cb1b9cda44c
Pavel Denisov <pv.denisov@interfax.ru>
2025-07-20 17:00:33 +0700

    add UI framework

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
+import { TuiRoot } from "@taiga-ui/core";
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TuiSliderComponent} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TuiSliderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true,
-
 })
 export class App {
-  protected readonly title = signal('cbt-log');
 }
