fa4daecf4aff722b7ef5cde49e1b73f1f8336083
Pavel Denisov <pv.denisov@interfax.ru>
2025-07-21 13:28:09 +0700

    add tui-root + minor update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
-import { TuiRoot } from "@taiga-ui/core";
+import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {TuiSliderComponent} from '@taiga-ui/kit';
-import {Emotion} from './components/emotions/emotion/emotion';
-import {Emotions} from './components/emotions/emotions';
+import { Emotions } from './components/emotions/emotions';
 
 @Component({
   selector: 'app-root',
-  imports: [ Emotions],
+  imports: [TuiRoot, Emotions],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss', './app.less'],
   standalone: true,
 })
 export class App {}
